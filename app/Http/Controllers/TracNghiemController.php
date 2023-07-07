f91842a6030dcc1ce35249d7b72037ee584eb179
B2014738 <ghib2014738@student.ctu.edu.vn>
2023-07-07 21:51:43 +0700

    tracnghiem

--- app/Http/Controllers/TracNghiemController.php
+++ app/Http/Controllers/TracNghiemController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
-
 use Illuminate\Http\Request;
+use App\Models\CategoryPost;
+use App\Models\Post;
 
 class TracNghiemController extends Controller
 {
     public function TracNghiem(){
-        return view('pages.tracnghiem');
+        $category = CategoryPost::all();
+        return view('pages.tracnghiem')->with(compact('category'));
     }
 }
